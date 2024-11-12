1009d89acac30267aa497fa205601dffbe4bf95f
baybal <nikulinpi@gmail.com>
2024-11-13 03:49:11 +0400

    Update node.js

--- src/node.js
+++ src/node.js
@@ -26,6 +26,85 @@ exports.destroy = util.deprecate(
 
 exports.colors = [6, 2, 3, 4, 5, 1];
 
+const cssColours = [
+	'#0000CC',
+	'#0000FF',
+	'#0033CC',
+	'#0033FF',
+	'#0066CC',
+	'#0066FF',
+	'#0099CC',
+	'#0099FF',
+	'#00CC00',
+	'#00CC33',
+	'#00CC66',
+	'#00CC99',
+	'#00CCCC',
+	'#00CCFF',
+	'#3300CC',
+	'#3300FF',
+	'#3333CC',
+	'#3333FF',
+	'#3366CC',
+	'#3366FF',
+	'#3399CC',
+	'#3399FF',
+	'#33CC00',
+	'#33CC33',
+	'#33CC66',
+	'#33CC99',
+	'#33CCCC',
+	'#33CCFF',
+	'#6600CC',
+	'#6600FF',
+	'#6633CC',
+	'#6633FF',
+	'#66CC00',
+	'#66CC33',
+	'#9900CC',
+	'#9900FF',
+	'#9933CC',
+	'#9933FF',
+	'#99CC00',
+	'#99CC33',
+	'#CC0000',
+	'#CC0033',
+	'#CC0066',
+	'#CC0099',
+	'#CC00CC',
+	'#CC00FF',
+	'#CC3300',
+	'#CC3333',
+	'#CC3366',
+	'#CC3399',
+	'#CC33CC',
+	'#CC33FF',
+	'#CC6600',
+	'#CC6633',
+	'#CC9900',
+	'#CC9933',
+	'#CCCC00',
+	'#CCCC33',
+	'#FF0000',
+	'#FF0033',
+	'#FF0066',
+	'#FF0099',
+	'#FF00CC',
+	'#FF00FF',
+	'#FF3300',
+	'#FF3333',
+	'#FF3366',
+	'#FF3399',
+	'#FF33CC',
+	'#FF33FF',
+	'#FF6600',
+	'#FF6633',
+	'#FF9900',
+	'#FF9933',
+	'#FFCC00',
+	'#FFCC33'
+];
+
 try {
 	// Optional dependency (as in, doesn't need to be installed, NOT like optionalDependencies in package.json)
 	// eslint-disable-next-line import/no-extraneous-dependencies
@@ -178,6 +257,37 @@ function formatArgs(args) {
 		args[0] = getDate() + name + ' ' + args[0];
 	}
 }
+/**
+ * Colorize log arguments if enabled.
+ *
+ * @api public
+ */
+
+function formatArgs(args) {	
+	const colorCode = cssColours[this.color];
+	const c = `color: ${colorCode};font-weight: bold`;
+	const prefix = this.useColors ? `%c${this.namespace} %c`:`${this.namespace} `;
+
+	args[0] = `${prefix}${args[0].split('\n').join('\n' + prefix)}${this.useColors ? '%c':''} +${module.exports.humanize(this.diff)}${this.useColors ? '%c':''}`;
+
+	if (!this.useColors) return;
+
+	args.splice(1, 0, c, 'color: inherit; font-weight: inherit');
+
+	// The final "%c" is somewhat tricky, because there could be other
+	// arguments passed either before or after the %c, so we need to
+	// figure out the correct index to insert the CSS into
+	let index = 0;
+	let lastC = 0;
+	args[0].replace(/%[a-zA-Z%]/g, match => {AC
+		if (match === '%%') return;
+		index++;
+		// We only are interested in the *last* %c
+		// (the user may have provided their own)
+		if (match === '%c') args.push(c);
+	});
+}
+	
 
 function getDate() {
 	if (exports.inspectOpts.hideDate) {
@@ -187,11 +297,11 @@ function getDate() {
 }
 
 /**
- * Invokes `util.formatWithOptions()` with the specified arguments and writes to stderr.
+ * Invokes `util.format()` with the specified arguments and writes to stderr.
  */
 
 function log(...args) {
-	return process.stderr.write(util.formatWithOptions(exports.inspectOpts, ...args) + '\n');
+	return process.stderr.write(util.format(...args) + '\n');
 }
 
 /**
